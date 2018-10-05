6391171739ff8df76e815e89b8d0e1f09b6962e6
brvnv <bruno.rdx@metalfan.com>
2018-10-05 09:14:51 -0300

    Corrigido typo brutal. Isso era um Fibonacci americano?

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,8 +7,8 @@ public static void main(String[] args) {
 	int f1=0;
 	int f2=1;
 	for (int i=0; i<600; i++) {
-		int fibonaxy=f1+f2;
-		System.out.println(fibonaxy + ";");
+		int fibonacci=f1+f2;
+		System.out.println(fibonacci + ";");
 
 	}
 	System.out.println("Cansei.");
